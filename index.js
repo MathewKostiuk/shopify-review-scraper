c16e53d0c93754e5b82de8d586c8793677419e77
Mathew Kostiuk <mathewkostiuk@gmail.com>
2020-05-09 15:09:35 +0900

    Added in node-cron to handle scheduled crawling + added logic to only crawl when necessary

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 const port = process.env.PORT || 3000
 
 const DataHelpers = require('./lib/data-helpers');
-const ScraperHelpers = require('./lib/utils/scraper-helpers');
-const themesRoutes = require('./routes/themes')(DataHelpers, ScraperHelpers);
+const themesRoutes = require('./routes/themes')(DataHelpers);
+
+const cron = require('node-cron');
+const CronHelpers = require("./lib/cron-helpers");
 
 app.use('/themes', themesRoutes);
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+cron.schedule('1 * * * *', () => {
+  console.log('cronning every hour', Date.now())
+  CronHelpers.checkForNewReviews();
+})
--- routes/themes.js
+++ routes/themes.js
@@ -2,14 +2,9 @@ const express = require('express');
 const themesRoutes = express.Router();
 
 
-module.exports = (DataHelpers, ScraperHelpers) => {
+module.exports = (DataHelpers) => {
   themesRoutes.get('/', async (req, res) => {
     const themes = await DataHelpers.getAllThemes();
-    const rpOptions = ScraperHelpers.generateRpOptions(themes);
-    rpOptions.forEach((rpOption, index) => {
-      const theme = themes[index];
-      ScraperHelpers.scrapeTheme(rpOption, theme);
-    })
     res.json({ themes });
   })
 
