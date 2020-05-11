6e482246f6596c73d63f220dc7603426eb418d70
Mathew Kostiuk <mathewkostiuk@gmail.com>
2020-05-11 17:43:31 +0900

    tidied up code + refactored to handle Promises better

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const DataHelpers = require('./lib/data-helpers');
 const themesRoutes = require('./routes/themes')(DataHelpers);
 
 const cron = require('node-cron');
-const CronHelpers = require("./lib/cron-helpers");
+const { checkForNewReviews } = require("./lib/cron-helpers");
 
 app.use('/themes', themesRoutes);
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
-cron.schedule('1 * * * *', () => {
-  console.log('cronning every hour', Date.now())
-  CronHelpers.checkForNewReviews();
+cron.schedule('* * * * *', async () => {
+  const newReviews = await checkForNewReviews();
+  console.log(newReviews);
 })
+
