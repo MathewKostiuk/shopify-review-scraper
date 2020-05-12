44397df8556bd5c0b1a52cea62b8ea94b926066a
Mathew Kostiuk <mathewkostiuk@gmail.com>
2020-05-12 13:01:10 +0900

    add route to get specific theme's reviews

--- routes/themes.js
+++ routes/themes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const themesRoutes = express.Router();
 
-
 module.exports = (DataHelpers) => {
   themesRoutes.get('/', async (req, res) => {
     const themes = await DataHelpers.getAllThemes();
     res.json({ themes });
+  })
+
+  themesRoutes.get('/:theme', async (req, res) => {
+    const themeReviews = await DataHelpers.getReviews(req.params.theme);
+    res.json({ themeReviews });
   })
 
   return themesRoutes;
