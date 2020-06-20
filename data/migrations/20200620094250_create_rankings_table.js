92afc49b795fc87631e6d3af039ca13661b2dd7b
Mathew Kostiuk <mathewkostiuk@gmail.com>
2020-06-20 09:58:19 +0900

    Set up structure for new rankings table

---
+++ data/migrations/20200620094250_create_rankings_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('rankings', table => {
+        table.increments('rankingId');
+        table.integer('rank').unsigned();
+        table.integer('themeId').unsigned().notNullable();
+        table.date('date');
+        table.foreign('themeId').references('themeId').inTable('themes');
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('rankings');
+};
