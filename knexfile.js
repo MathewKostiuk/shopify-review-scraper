8f3c15347605b9232d8c0318d4baf31fc59e4336
MathewKostiuk <mathewkostiuk@gmail.com>
2021-03-16 19:11:16 +0900

    Revert previous change

--- knexfile.js
+++ knexfile.js
@@ -36,7 +36,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
-    rejectUnauthorized: false,
+    ssl: true,
     pool: {
       min: 2,
       max: 10
